5f9b4c3c647d0ed84c66431cb319458b262ed814
AngelStep-ux <alina.step@mail.ru>
2025-08-20 12:43:10 +0300

    Исключить orders/email_settings.py из репозитория

--- orders/email_settings.py
+++
@@ -1,6 +0,0 @@
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.mail.ru'
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'alina.step@mail.ru'
-EMAIL_HOST_PASSWORD = '725090Step'
